b67fa72ebbfaa3918c38fc3e36ebc1b1e40cdba3
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2025-12-17 10:50:31 -0700

    adding initial definition of annotations

---
+++ research-assistant-extension/models/annotations.js
@@ -0,0 +1,19 @@
+// From chapter 2.3 - Annotation: The Core of Active Reading - of the Electronic Book by CC Marshall
+
+// Annotation types:
+
+// Telegraphic marks: Underlines, circles and asterisks (for now we will focus on underlines)
+
+// Explicit Notes: Special kind of Notes (Writing words or sentences in the margin)
+/**
+ * - Appears exactly where note wants to be created on web page, exactly how margin notes would appear in books
+ * - Has the same properties as Notes
+ */
+
+// Header/Footer/Title Marks: Special kind of ExplicitNotes (Readers often circle chapter titles or headings to map the structure of the argument.)
+/**
+ * - Exhibits the same behaviour as explicit notes
+ * - Mostly for titles (Heading-like elements for instance), header and footers on PageBreaks
+ */
+
+// [web]PageBreaks + Special scrollbar (TODO: Check notes for exact specification on this one)
